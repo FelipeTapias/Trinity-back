57b9ad5820feb9444fc220fdd102fd7f2e966b64
Felipe Tapias <atapias@sistecredito.com>
2025-01-02 15:37:24 -0500

    Implementacion de interceptor para las respuesta base en el controlador de customerUsuario

--- Src/Initialization/RestService.Trinity/Controllers/CustomerUserController.cs
+++ Src/Initialization/RestService.Trinity/Controllers/CustomerUserController.cs
@@ -1,72 +1,87 @@
 using Aplication.Interfaces.Application;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
+using RestService.Trinity.Controllers.Base;
 
 namespace RestService.Trinity.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CustomerUserController : ControllerBase
+    public class CustomerUserController : BaseController
     {
         private readonly ILogger<CustomerUserController> _logger;
         private readonly ICustomerUserService _userService;
 
-        public CustomerUserController(ILogger<CustomerUserController> logger, ICustomerUserService userService)
+        public CustomerUserController(ILogger<CustomerUserController> logger, ICustomerUserService userService): base(logger)
         {
             _logger = logger;
             _userService = userService;
         }
 
         [HttpPost("CreateCustomerUser", Name = "CreateCustomerUser")]
-        public async Task<string> Post(CustomerUser user)
+        public async Task<IActionResult> Post(CustomerUser customerUser)
         {
             _logger.LogInformation("Creando el cliente usuario");
-            string idReturn = await _userService.CreateUser(user);
-            _logger.LogInformation($"Se creo el cliente usuario con el ID: {idReturn}");
-            return idReturn;
+            return await HandleResponse(async () =>
+            {
+                return await _userService.CreateUser(customerUser);
+            }, "Usuario creado correctamente");
         }
 
         [HttpGet("GetAllCustomerUsers", Name = "GetAllCustomerUsers")]
-        public async Task<IEnumerable<CustomerUser>> GetAllUser()
+        public async Task<IActionResult> GetAllUser()
         {
             _logger.LogInformation("Obteniendo todos los clientes usuarios");
-            return await _userService.GetAllUser();
+            return await HandleResponse(async () =>
+            {
+                return await _userService.GetAllUser();
+            }, "Usuarios obtenidos correctamente");
         }
 
         [HttpGet("GetCustomerUserById", Name = "GetCustomerUserById")]
-        public async Task<CustomerUser> GetUserById([FromQuery] string customerId)
+        public async Task<IActionResult> GetUserById([FromQuery] string customerId)
         {
             _logger.LogInformation($"Obteneindo cliente usuario con id: {customerId}");
-            return await _userService.GetUserById(customerId);
+            return await HandleResponse(async () =>
+            {
+                return await _userService.GetUserById(customerId);
+            }, $"Cliente usuario con CustomerId {customerId} obtenido correctamente");
         }
 
         [HttpDelete("DeleteCustomerUserById", Name = "DeleteCustomerUserById")]
-        public async Task<string> DeleteUserById([FromQuery] string customerId)
+        public async Task<IActionResult> DeleteUserById([FromQuery] string customerId)
         {
             _logger.LogInformation($"Eliminando usuario con id: {customerId}");
-            return await _userService.DeleteUserById(customerId);
+            return await HandleResponse(async () => {
+                return await _userService.DeleteUserById(customerId);
+            }, $"Usuario con Id {customerId} eliminado correctamente");
         }
 
         [HttpPut("EditCustomerUserById", Name = "EditCustomerUserById")]
-        public async Task<string> UpdateUserById(string customerId, CustomerUser user)
+        public async Task<IActionResult> UpdateUserById(string customerId, CustomerUser customerUser)
         {
             _logger.LogInformation($"Actualizando usuario con id: {customerId}");
-            await _userService.UpdateUserById(customerId, user);
-            return customerId;
+            return await HandleResponse(async () => {
+                return await _userService.UpdateUserById(customerId, customerUser);
+            }, $"Usuario con Id {customerId} actualizado correctamente");
         }
 
         [HttpGet("GetCustomerUserByDocumentNumber", Name = "GetCustomerUserByDocumentNumber")]
-        public async Task<CustomerUser> GetUserByIdDocument([FromQuery] string documentNumber)
+        public async Task<IActionResult> GetUserByIdDocument([FromQuery] string documentNumber)
         {
             _logger.LogInformation($"Obteniendo cliente usuario con DocumentNumber: {documentNumber}");
-            return await _userService.GetUserByIdDocument(documentNumber);
+            return await HandleResponse(async () => {
+                return await _userService.GetUserByIdDocument(documentNumber);
+            }, $"Cliente usuario con IdDocument {documentNumber} obtenido correctamente");
         }
 
         [HttpGet("GetIdByDocumentNumber", Name = "GetIdByDocumentNumber")]
-        public async Task<string> GetIdByIdDocument([FromQuery] string documentNumber)
+        public async Task<IActionResult> GetIdByIdDocument([FromQuery] string documentNumber)
         {
             _logger.LogInformation($"Obteniendo CustomerId con DocumentNumber: {documentNumber}");
-            return await _userService.GetIdByIdDocument(documentNumber);
+            return await HandleResponse(async () => {
+                return await _userService.GetIdByIdDocument(documentNumber);
+            }, $"CustomerId de usuario con DocumentNumber {documentNumber} obtenido correctamente");
         }
     }
 }
